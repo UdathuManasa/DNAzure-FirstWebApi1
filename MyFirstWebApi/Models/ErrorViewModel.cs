40f27b159f99116574f1bdc76b958de569bd85af
Udathu Manasa <UdathuManasa@outlook.com>
2021-09-14 16:17:09 +0530

    Add project files.

---
+++ MyFirstWebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFirstWebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
